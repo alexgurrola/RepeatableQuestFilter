e83178078c201eaeaa824a19eda560c3a4b24f6f
alexgurrola <alexgurrola1@gmail.com>
2018-05-05 17:45:55 -0700

    Add Nested Controls & Settings (WIP)
    Signed-off-by: alexgurrola <alexgurrola1@gmail.com>

--- RepeatableQuestFilter.lua
+++ RepeatableQuestFilter.lua
@@ -4,16 +4,41 @@
 
 RepeatableQuestFilter = {}
 RepeatableQuestFilter.name = "RepeatableQuestFilter"
-RepeatableQuestFilter.configVersion = 2
+RepeatableQuestFilter.configVersion = 3
+RepeatableQuestFilter.controls = {
+    KillSpree = {
+        AldmeriDominion = {},
+        DaggerfallCovenant = {},
+        EbonheartPact = {},
+    }
+}
 RepeatableQuestFilter.defaults = {
     ThievesGuild = true,
     DarkBrotherhood = true,
     CrimeSpree = true,
     Launder = true,
-    KillSpreeGC = true,
-    KillSpreeAD = true,
-    KillSpreeDC = true,
-    KillSpreeEP = true,
+    KillSpree = {
+        -- Gold Coast
+        GoldCoast = true,
+        -- Aldmeri Dominion
+        Auridon = true,
+        Grahtwood = true,
+        Greenshade = true,
+        MalabalTor = true,
+        ReapersMarch = true,
+        -- Daggerfall Covenant
+        AlikrDesert = true,
+        Bangkorai = true,
+        Glenumbra = true,
+        Rivenspire = true,
+        Stormhaven = true,
+        -- Ebonheart Pact
+        Deshaan = true,
+        Eastmarch = true,
+        Shadowfen = true,
+        Stonefalls = true,
+        TheRift = true,
+    }
 }
 RepeatableQuestFilter.filters = {}
 
@@ -33,7 +58,7 @@ end
 --------------------------
 
 function RepeatableQuestFilter:Initialize(self)
-    self.saveData = ZO_SavedVars:New(self.name.."Data", self.configVersion, nil, self.defaults)
+    self.saveData = ZO_SavedVars:New(self.name .. "Data", self.configVersion, nil, self.defaults)
     self:RepairSaveData(self)
     self:CreateSettingsWindow(self)
     self:BuildFilters(self)
@@ -63,10 +88,17 @@ end
 
 function RepeatableQuestFilter:RepairSaveData(self)
     for key, value in pairs(self.defaults) do
-        if(self.saveData[key] == nil) then
+        if (self.saveData[key] == nil) then
             self.saveData[key] = value
         end
     end
+    --[[
+    for key, value in pairs(self.defaults.KillSpree) do
+        if (self.saveData.KillSpree[key] == nil) then
+            self.saveData.KillSpree[key] = value
+        end
+    end
+    ]]
 end
 
 ------------------
@@ -100,28 +132,28 @@ function RepeatableQuestFilter:BuildFilters(self)
     self.filters.dialog = {
         -- Tip Board
         ["Some new fa"] = self.saveData.ThievesGuild, -- The Cutpurse's Craft (Required to get Dailies)
-        ["Rumors that"] = self.saveData.CrimeSpree, -- Any Crime Spree
+        ["Rumors that"] = self.saveData.CrimeSpree, -- Crime Spree
         ["Esteemed th"] = self.saveData.Launder, -- The Covetous Countess
         -- Marked for Death
-        ["Demand for "] = self.saveData.KillSpreeGC, -- Gold Coast
+        ["Demand for "] = self.saveData.KillSpree.GoldCoast, -- Gold Coast
         -- Aldmeri Dominion
-        ["The Thalmor"] = self.saveData.KillSpreeAD, -- Auridon
-        ["Back to the"] = self.saveData.KillSpreeAD, -- Grahtwood
-        ["The damn El"] = self.saveData.KillSpreeAD, -- Greenshade
-        ["Malabal Tor"] = self.saveData.KillSpreeAD, -- Malabal Tor
-        ["This one do"] = self.saveData.KillSpreeAD, -- Reaper's March
+        ["The Thalmor"] = self.saveData.KillSpree.Auridon, -- Auridon
+        ["Back to the"] = self.saveData.KillSpree.Grahtwood, -- Grahtwood
+        ["The damn El"] = self.saveData.KillSpree.Greenshade, -- Greenshade
+        ["Malabal Tor"] = self.saveData.KillSpree.MalabalTor, -- Malabal Tor
+        ["This one do"] = self.saveData.KillSpree.ReapersMarch, -- Reaper's March
         -- Daggerfall Covenant
-        ["The Redguar"] = self.saveData.KillSpreeDC, -- Alik'r Desert
-        ["Strained re"] = self.saveData.KillSpreeDC, -- Bangkorai
-        ["Trade is th"] = self.saveData.KillSpreeDC, -- Glenumbra
-        ["The Covenan"] = self.saveData.KillSpreeDC, -- Rivenspire
-        ["Smuggling i"] = self.saveData.KillSpreeDC, -- Stormhaven
+        ["The Redguar"] = self.saveData.KillSpree.AlikrDesert, -- Alik'r Desert
+        ["Strained re"] = self.saveData.KillSpree.Bangkorai, -- Bangkorai
+        ["Trade is th"] = self.saveData.KillSpree.Glenumbra, -- Glenumbra
+        ["The Covenan"] = self.saveData.KillSpree.Rivenspire, -- Rivenspire
+        ["Smuggling i"] = self.saveData.KillSpree.Stormhaven, -- Stormhaven
         -- Ebonheart Pact
-        ["Never forgi"] = self.saveData.KillSpreeEP, -- Deshaan
-        ["The Thanes "] = self.saveData.KillSpreeEP, -- Eastmarch
-        ["The Ebonhea"] = self.saveData.KillSpreeEP, -- Shadowfen
-        ["Brothers an"] = self.saveData.KillSpreeEP, -- Stonefalls
-        ["My exile le"] = self.saveData.KillSpreeEP, -- The Rift
+        ["Never forgi"] = self.saveData.KillSpree.Deshaan, -- Deshaan
+        ["The Thanes "] = self.saveData.KillSpree.Eastmarch, -- Eastmarch
+        ["The Ebonhea"] = self.saveData.KillSpree.Shadowfen, -- Shadowfen
+        ["Brothers an"] = self.saveData.KillSpree.Stonefalls, -- Stonefalls
+        ["My exile le"] = self.saveData.KillSpree.TheRift, -- The Rift
     }
 end
 
@@ -135,25 +167,29 @@ function RepeatableQuestFilter:CreateSettingsWindow(self)
         name = "Quest Filter",
         displayName = "Repeatable Quest Filter",
         author = "Positron",
-        version = "1.1",
+        version = "1.2",
         website = "https://github.com/alexgurrola/RepeatableQuestFilter",
         slashCommand = "/questfilter",
         registerForDefaults = true,
     }
-    local panel = LAM2:RegisterAddonPanel(self.name.."Config", panelData)
+    -- local panel =
+    LAM2:RegisterAddonPanel(self.name .. "Config", panelData)
     local optionsData = {}
     optionsData[#optionsData + 1] = {
         type = "header",
         name = "Thieves Guild Settings"
     }
-    --optionsData[#optionsData + 1] = {
-    --    type = "description",
-    --    text = "Enable focused farming or leveling by filtering repeatable quests."
-    --}
+    --[[
+    optionsData[#optionsData + 1] = {
+        type = "description",
+        text = "Enable focused farming or leveling by filtering repeatable quests."
+    }
+    ]]
     optionsData[#optionsData + 1] = {
         type = "checkbox",
         name = "Filters",
         tooltip = "Turn this off if you want to allow all Thieves Guild Quests through.",
+        default = self.defaults.ThievesGuild,
         getFunc = function()
             return self.saveData.ThievesGuild
         end,
@@ -165,6 +201,7 @@ function RepeatableQuestFilter:CreateSettingsWindow(self)
         type = "checkbox",
         name = "Crime Spree",
         tooltip = "Turn this on if you want to allow Thieves Guild Crime Sprees through.",
+        default = self.defaults.CrimeSpree,
         getFunc = function()
             return self.saveData.CrimeSpree
         end,
@@ -176,6 +213,7 @@ function RepeatableQuestFilter:CreateSettingsWindow(self)
         type = "checkbox",
         name = "Laundering",
         tooltip = "Turn this on if you want to allow The Covetous Countess through.",
+        default = self.defaults.Launder,
         getFunc = function()
             return self.saveData.Launder
         end,
@@ -192,6 +230,7 @@ function RepeatableQuestFilter:CreateSettingsWindow(self)
         type = "checkbox",
         name = "Filters",
         tooltip = "Turn this off if you want to allow all Dark Brotherhood Quests through.",
+        default = self.defaults.DarkBrotherhood,
         getFunc = function()
             return self.saveData.DarkBrotherhood
         end,
@@ -201,49 +240,74 @@ function RepeatableQuestFilter:CreateSettingsWindow(self)
     }
     optionsData[#optionsData + 1] = {
         type = "checkbox",
-        name = "Gold Coast Kill Sprees",
+        name = "Gold Coast Kill Spree",
         tooltip = "Turn this on if you want to allow Gold Coast Kill Sprees through.",
+        default = self.defaults.KillSpree.GoldCoast,
         getFunc = function()
-            return self.saveData.KillSpreeGC
+            return self.saveData.KillSpree.GoldCoast
         end,
         setFunc = function(newValue)
-            self.saveData.KillSpreeGC = newValue
+            self.saveData.KillSpree.GoldCoast = newValue
         end,
     }
+    -- Aldmeri Dominion
     optionsData[#optionsData + 1] = {
-        type = "checkbox",
+        type = "submenu",
         name = "Aldmeri Dominion Kill Sprees",
-        tooltip = "Turn this on if you want to allow Aldmeri Dominion Kill Sprees through.",
+        tooltip = "Decide which places you wish to have Kill Sprees available in the Aldmeri Dominion.",
+        controls = self.controls.KillSpree.AldmeriDominion
+    }
+    self.controls.KillSpree.AldmeriDominion[#self.controls.KillSpree.AldmeriDominion + 1] = {
+        type = "checkbox",
+        name = "Auridon Kill Sprees",
+        tooltip = "Turn this on if you want to allow Auridon Kill Sprees through.",
+        default = self.defaults.KillSpree.Auridon,
         getFunc = function()
-            return self.saveData.KillSpreeAD
+            return self.saveData.KillSpree.Auridon
         end,
         setFunc = function(newValue)
-            self.saveData.KillSpreeAD = newValue
+            self.saveData.KillSpree.Auridon = newValue
         end,
     }
+    -- Daggerfall Covenant
     optionsData[#optionsData + 1] = {
-        type = "checkbox",
+        type = "submenu",
         name = "Daggerfall Covenant Kill Sprees",
-        tooltip = "Turn this on if you want to allow Daggerfall Covenant Kill Sprees through.",
-        getFunc = function()
-            return self.saveData.KillSpreeDC
-        end,
-        setFunc = function(newValue)
-            self.saveData.KillSpreeDC = newValue
-        end,
+        tooltip = "Decide which places you wish to have Kill Sprees available in the Daggerfall Covenant.",
+        controls = self.controls.KillSpree.DaggerfallCovenant
     }
-    optionsData[#optionsData + 1] = {
+    self.controls.KillSpree.DaggerfallCovenant[#self.controls.KillSpree.DaggerfallCovenant + 1] = {
         type = "checkbox",
-        name = "Ebonheart Pact Kill Sprees",
-        tooltip = "Turn this on if you want to allow Ebonheart Pact Kill Sprees through.",
+        name = "Alik'r Desert Kill Sprees",
+        tooltip = "Turn this on if you want to allow Alik'r Desert Kill Sprees through.",
+        default = self.defaults.KillSpree.AlikrDesert,
         getFunc = function()
-            return self.saveData.KillSpreeEP
+            return self.saveData.KillSpree.AlikrDesert
         end,
         setFunc = function(newValue)
-            self.saveData.KillSpreeEP = newValue
+            self.saveData.KillSpree.AlikrDesert = newValue
         end,
     }
-    LAM2:RegisterOptionControls(self.name.."Config", optionsData)
+    -- Ebonheart Pact
+    optionsData[#optionsData + 1] = {
+        type = "submenu",
+        name = "Ebonheart Pact Kill Sprees",
+        tooltip = "Decide which places you wish to have Kill Sprees available in the Ebonheart Pact.",
+        controls = self.controls.KillSpree.EbonheartPact
+    }
+    self.controls.KillSpree.EbonheartPact[#self.controls.KillSpree.EbonheartPact + 1] = {
+        type = "checkbox",
+        name = "Deshaan Kill Sprees",
+        tooltip = "Turn this on if you want to allow Deshaan Kill Sprees through.",
+        default = self.defaults.ThievesGuild,
+        getFunc = function()
+            return self.saveData.KillSpree.Deshaan
+        end,
+        setFunc = function(newValue)
+            self.saveData.KillSpree.Deshaan = newValue
+        end,
+    }
+    LAM2:RegisterOptionControls(self.name .. "Config", optionsData)
 end
 
 ------------------------
@@ -262,14 +326,14 @@ function RepeatableQuestFilter:OverwritePopulateChatterOption(self, interaction)
         end
         -- the player has to be on an enabled map
         if not _self.filters.zones[GetZoneId(GetUnitZoneIndex("player"))] then
-          PopulateChatterOption(self, index, fun, txt, type, ...)
-          return
+            PopulateChatterOption(self, index, fun, txt, type, ...)
+            return
         end
         -- check if the current dialog starts an enabled quest
         local offerText = GetOfferedQuestInfo()
         if string.len(offerText) == 0 then
-          PopulateChatterOption(self, index, fun, txt, type, ...)
-          return
+            PopulateChatterOption(self, index, fun, txt, type, ...)
+            return
         end
         if not _self.filters.dialog[string.sub(offerText, 2, 12)] then
             -- if it is a different quest, only display the goodbye option
